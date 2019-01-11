3a8a8001f132847bf0a0583800900ad25781e2f7
Tim Bewsey <tim@h2s.co.za>
2019-01-11 10:42:49 +0200

    Updated Dependancies

--- test/logger_test.js
+++ test/logger_test.js
@@ -29,7 +29,12 @@ describe('Logger', function() {
     });
 
     it('defaults config', function() {
+
       Logger.configure();
+      //console.log("LOGGER:::",Logger)
+      console.log("LOGGER CONFIG:::", JSON.stringify(Logger.config,
+        null, 2));
+
       Logger.config.should.eql({
         logCacheSize: 50,
         logComponents: [],
@@ -49,14 +54,21 @@ describe('Logger', function() {
         logFileNameAbsolute: true,
         logWriter: Logger.config.logWriter, // dodge object
         logger: {
-          appenders: [{
-            layout: {
-              pattern: '[%[%5.5p%]] - %m',
-              type: 'pattern'
-            },
-            makers: Logger.config.logger.appenders[0].makers, // dodge object
-            type: 'console'
-          }]
+          appenders: {
+            console: {
+              type: "console",
+              layout: {
+                type: "pattern",
+                pattern: "[%[%5.5p%]] - %m"
+              }
+            }
+          },
+          categories: {
+            default: {
+              appenders: ["console"],
+              level: "info"
+            }
+          }
         },
         log: Logger.config.log // dodge function
       })
@@ -64,20 +76,21 @@ describe('Logger', function() {
 
   });
 
-  context('log functions', function(){
+  context('log functions', function() {
 
-    it('defines a function to emit a log message at each level', function() {
-      Logger.configure();
-      var log = Logger.createLogger();
-      log.fatal   .should.be.an.instanceof(Function);
-      log.error   .should.be.an.instanceof(Function);
-      log.warn    .should.be.an.instanceof(Function);
-      log.info    .should.be.an.instanceof(Function);
-      log.debug   .should.be.an.instanceof(Function);
-      log.$$DEBUG .should.be.an.instanceof(Function);
-      log.trace   .should.be.an.instanceof(Function);
-      log.$$TRACE .should.be.an.instanceof(Function);
-    });
+    it('defines a function to emit a log message at each level',
+      function() {
+        Logger.configure();
+        var log = Logger.createLogger();
+        log.fatal.should.be.an.instanceof(Function);
+        log.error.should.be.an.instanceof(Function);
+        log.warn.should.be.an.instanceof(Function);
+        log.info.should.be.an.instanceof(Function);
+        log.debug.should.be.an.instanceof(Function);
+        log.$$DEBUG.should.be.an.instanceof(Function);
+        log.trace.should.be.an.instanceof(Function);
+        log.$$TRACE.should.be.an.instanceof(Function);
+      });
 
     it('calls the log function', function(done) {
       Logger.configure();
@@ -92,37 +105,48 @@ describe('Logger', function() {
       Logger.configure();
       var called = false;
       var log = Logger.createLogger();
-      
+
       Logger.config.logWriter.isInfoEnabled = done;
       log.info('message');
     });
 
     it('formats the log text from multiple arguments', function(done) {
       Logger.configure();
-      Logger.config.log = function(level, context, component, message) {
-        message.should.equal('string: STRING, number NUMBER, json {"json":"data"}');
+      Logger.config.log = function(level, context, component,
+        message) {
+        message.should.equal(
+          'string: STRING, number NUMBER, json {"json":"data"}'
+        );
         done();
       }
       var log = Logger.createLogger();
-      log.info('string: %s, number %s, json %j', 'STRING', 'NUMBER', {json: 'data'});
+      log.info('string: %s, number %s, json %j', 'STRING', 'NUMBER', {
+        json: 'data'
+      });
     });
 
-    it('includes the array of log arguments in log call if logStackTraces is enabled', function(done) {
-      Logger.configure({
-        logStackTraces: true,
+    it(
+      'includes the array of log arguments in log call if logStackTraces is enabled',
+      function(done) {
+        Logger.configure({
+          logStackTraces: true,
+        });
+        Logger.config.log = function(level, context, component,
+          message, array) {
+          array.should.eql([
+            'string: %s, number %s, json %j',
+            'STRING',
+            'NUMBER', {
+              json: 'data'
+            }
+          ]);
+          done();
+        }
+        var log = Logger.createLogger();
+        log.info('string: %s, number %s, json %j', 'STRING', 'NUMBER', {
+          json: 'data'
+        });
       });
-      Logger.config.log =  function(level, context, component, message, array) {
-        array.should.eql([
-          'string: %s, number %s, json %j',
-          'STRING',
-          'NUMBER',
-          {json: 'data'}
-        ]);
-        done();
-      }
-      var log = Logger.createLogger();
-      log.info('string: %s, number %s, json %j', 'STRING', 'NUMBER', {json: 'data'});
-    });
 
     it('default log to debug', function() {
       Logger.configure();
@@ -138,14 +162,14 @@ describe('Logger', function() {
       var obj = {};
       Logger.configure()
       Logger.createLogger('component', obj);
-      obj.fatal   .should.be.an.instanceof(Function);
-      obj.error   .should.be.an.instanceof(Function);
-      obj.warn    .should.be.an.instanceof(Function);
-      obj.info    .should.be.an.instanceof(Function);
-      obj.debug   .should.be.an.instanceof(Function);
-      obj.$$DEBUG .should.be.an.instanceof(Function);
-      obj.trace   .should.be.an.instanceof(Function);
-      obj.$$TRACE .should.be.an.instanceof(Function);
+      obj.fatal.should.be.an.instanceof(Function);
+      obj.error.should.be.an.instanceof(Function);
+      obj.warn.should.be.an.instanceof(Function);
+      obj.info.should.be.an.instanceof(Function);
+      obj.debug.should.be.an.instanceof(Function);
+      obj.$$DEBUG.should.be.an.instanceof(Function);
+      obj.trace.should.be.an.instanceof(Function);
+      obj.$$TRACE.should.be.an.instanceof(Function);
     });
 
   });
@@ -153,7 +177,9 @@ describe('Logger', function() {
   context('logging to console', function() {
 
     it('logs to console by default', function() {
-      Logger.configure({logLevel: 'info'});
+      Logger.configure({
+        logLevel: 'info'
+      });
       var log = Logger.createLogger('component');
       log.info('XXXXX');
     });
@@ -169,6 +195,7 @@ describe('Logger', function() {
       Logger.configure({
         logFile: 'file.log'
       });
+      console.log("LOGGER ::::", JSON.stringify(Logger))
       var log = Logger.createLogger('component');
       log.info('xxxxx');
       setTimeout(function() {
@@ -179,22 +206,24 @@ describe('Logger', function() {
       }, 100);
     });
 
-    it('can includes a stack trace if enabled and the last arg is an error', function(done) {
-      try {
-        fs.unlinkSync('file.log');
-      } catch (e) {}
-      Logger.configure({
-        logFile: 'file.log'
+    it(
+      'can includes a stack trace if enabled and the last arg is an error',
+      function(done) {
+        try {
+          fs.unlinkSync('file.log');
+        } catch (e) {}
+        Logger.configure({
+          logFile: 'file.log'
+        });
+        var log = Logger.createLogger('component');
+        log.info('xxxxx', new Error('Something'));
+        setTimeout(function() {
+          var logged = fs.readFileSync('file.log').toString();
+          logged.should.match(/ \[ INFO\] - Error: Something/);
+          fs.unlinkSync('file.log');
+          done();
+        }, 100);
       });
-      var log = Logger.createLogger('component');
-      log.info('xxxxx', new Error('Something'));
-      setTimeout(function() {
-        var logged = fs.readFileSync('file.log').toString();
-        logged.should.match(/ \[ INFO\] - Error: Something/);
-        fs.unlinkSync('file.log');
-        done();
-      }, 100);
-    });
 
   });
 
@@ -223,7 +252,7 @@ describe('Logger', function() {
       log1.info('message 1');
       log2.info('message 2');
       log3.info('message 3');
-      
+
       context.context = 'xxx';
 
       log1.info('message 1');
@@ -238,7 +267,9 @@ describe('Logger', function() {
 
       setTimeout(function() {
         var logged = fs.readFileSync('file.log').toString();
-        logged.should.equal('(component1) message 1\n(component2) message 2\n(component3) message 3\nxxx (component1) message 1\nxxx (component2) message 2\nxxx (component3) message 3\nyyy (component1) message 1\nyyy (component2) message 2\nyyy (component3) message 3\n');
+        logged.should.equal(
+          '(component1) message 1\n(component2) message 2\n(component3) message 3\nxxx (component1) message 1\nxxx (component2) message 2\nxxx (component3) message 3\nyyy (component1) message 1\nyyy (component2) message 2\nyyy (component3) message 3\n'
+        );
         fs.unlinkSync('file.log');
         done();
       }, 100);
@@ -254,7 +285,9 @@ describe('Logger', function() {
       Logger.emitter.on('before', function() {
         befores++;
       });
-      Logger.configure({logLevel: 'info'});
+      Logger.configure({
+        logLevel: 'info'
+      });
       var log = Logger.createLogger();
 
       befores.should.equal(0);
@@ -277,7 +310,9 @@ describe('Logger', function() {
       Logger.emitter.on('after', function() {
         afters++;
       });
-      Logger.configure({logLevel: 'info'});
+      Logger.configure({
+        logLevel: 'info'
+      });
       var log = Logger.createLogger();
 
       afters.should.equal(0);
@@ -305,7 +340,9 @@ describe('Logger', function() {
         logLevel: 'all'
       });
       var count = 0;
-      Date.now = function() {return count++}
+      Date.now = function() {
+        return count++;
+      }
 
       var log = Logger.createLogger();
       log.trace('A');
@@ -315,40 +352,35 @@ describe('Logger', function() {
       log.error('E');
       log.fatal('F');
 
-      Logger.cache.should.eql([
-        {
+      Logger.cache.should.eql([{
           context: undefined,
           component: undefined,
           level: 'fatal',
           message: 'F',
           timestamp: 5,
           timedelta: 1,
-        },
-        {
+        }, {
           context: undefined,
           component: undefined,
           level: 'error',
           message: 'E',
           timestamp: 4,
           timedelta: 1,
-        },
-        {
+        }, {
           context: undefined,
           component: undefined,
           level: 'warn',
           message: 'D',
           timestamp: 3,
           timedelta: 1,
-        },
-        {
+        }, {
           context: undefined,
           component: undefined,
           level: 'info',
           message: 'C',
           timestamp: 2,
           timedelta: 1,
-        },
-        {
+        }, {
           context: undefined,
           component: undefined,
           level: 'debug',
@@ -356,7 +388,7 @@ describe('Logger', function() {
           timestamp: 1,
           timedelta: 1,
         },
-        
+
       ])
 
     });
@@ -378,7 +410,7 @@ describe('Logger', function() {
       var component1 = Logger.createLogger('component1');
       var component2 = Logger.createLogger('component2');
       var component3 = Logger.createLogger('component3');
-      
+
       component1.info('aaaaa');
       component2.info('bbbbb');
       component3.info('ccccc');
@@ -393,52 +425,63 @@ describe('Logger', function() {
       }, 100);
     });
 
-    it('logs fatal, error and warn from even if filtered out', function(done) {
-      try {
-        fs.unlinkSync('file.log');
-      } catch (e) {}
-      Logger.configure({
-        logFile: 'file.log',
-        logComponents: ['component1']
+    it('logs fatal, error and warn from even if filtered out',
+      function(
+        done) {
+        try {
+          fs.unlinkSync('file.log');
+        } catch (e) {}
+        Logger.configure({
+          logFile: 'file.log',
+          logComponents: ['component1']
+        });
+
+        var component1 = Logger.createLogger('component1');
+        var component2 = Logger.createLogger('component2');
+        var component3 = Logger.createLogger('component3');
+        var component4 = Logger.createLogger('component4');
+
+        component2.fatal('aaaaa');
+        component3.error('bbbbb');
+        component4.warn('ccccc');
+
+        setTimeout(function() {
+          var logged = fs.readFileSync('file.log').toString();
+          logged.should.match(/\(component2\) aaaaa/);
+          logged.should.match(/\(component3\) bbbbb/);
+          logged.should.match(/\(component4\) ccccc/);
+          fs.unlinkSync('file.log');
+          done();
+        }, 100);
       });
-
-      var component1 = Logger.createLogger('component1');
-      var component2 = Logger.createLogger('component2');
-      var component3 = Logger.createLogger('component3');
-      var component4 = Logger.createLogger('component4');
-      
-      component2.fatal('aaaaa');
-      component3.error('bbbbb');
-      component4.warn('ccccc');
-
-      setTimeout(function() {
-        var logged = fs.readFileSync('file.log').toString();
-        logged.should.match(/\(component2\) aaaaa/);
-        logged.should.match(/\(component3\) bbbbb/);
-        logged.should.match(/\(component4\) ccccc/);
-        fs.unlinkSync('file.log');
-        done();
-      }, 100);
-    });
 
   });
 
   context('logger subconfig', function() {
 
-    it('allows for entirely externally defined log4js config', function(done) {
+    it('allows for entirely externally defined log4js config', function(
+      done) {
 
       Logger.configure({
         logMessageDelimiter: ' ',
         logTimeDelta: false,
         logger: {
-          appenders: [{
-            type: 'file',
-            filename: 'file.log',
-            layout: {
-              type: 'pattern',
-              pattern: '--- %m ---'
+          appenders: {
+            console: {
+              type: 'file',
+              filename: 'file.log',
+              layout: {
+                type: 'pattern',
+                pattern: '--- %m ---'
+              }
             }
-          }]
+          },
+          categories: {
+            default: {
+              appenders: ['console'],
+              level: "info"
+            }
+          }
         }
       });
 
@@ -461,33 +504,40 @@ describe('Logger', function() {
 
     this.timeout(5000);
 
-    it('allows for a listener to be inserted, checks the listener receives all input', function(done) {
+    it(
+      'allows for a listener to be inserted, checks the listener receives all input',
+      function(done) {
 
-      var listened = {};
+        var listened = {};
 
-      Logger.emitter.on('after', function(level, message) {
+        Logger.emitter.on('after', function(level, message) {
 
-        listened[level] = message;
-        if (Object.keys(listened).length == 3){
+          listened[level] = message;
+          if (Object.keys(listened).length == 3) {
 
-          listened['info'].split('\t')[1].should.equal('INFO-TEST');
-          listened['warn'].split('\t')[1].should.equal('WARN-TEST');
-          listened['error'].split('\t')[1].should.equal('ERROR-TEST');
+            listened['info'].split('\t')[1].should.equal(
+              'INFO-TEST');
+            listened['warn'].split('\t')[1].should.equal(
+              'WARN-TEST');
+            listened['error'].split('\t')[1].should.equal(
+              'ERROR-TEST');
 
-          done();
-        }
+            done();
+          }
+
+        });
+
+        Logger.configure({
+          logLevel: 'info'
+        });
+
+        var log = Logger.createLogger();
+
+        log.info('INFO-TEST');
+        log.warn('WARN-TEST');
+        log.error('ERROR-TEST');
 
       });
-
-      Logger.configure({logLevel: 'info'});
-
-      var log = Logger.createLogger();
-
-      log.info('INFO-TEST');
-      log.warn('WARN-TEST');
-      log.error('ERROR-TEST');
-
-    });
 
   });
 
